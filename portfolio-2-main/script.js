33045eda096d2ed925e3dd7c65c658e912b70c48
NICOLAS ANDRÉ KRAUSE DE MELLO <nicolas_a_mello@estudante.sesisenai.org.br>
2024-11-12 09:27:59 -0300

    1.0

---
+++ portfolio-2-main/script.js
@@ -0,0 +1,56 @@
+function materia() {
+    window.location.href = "materia.html"
+  }
+
+function link() {
+    window.location.href = "link.html"
+  }
+
+function sobre() {
+    window.location.href = "index.html"
+  }
+
+function trimestre() {
+    window.location.href = "trimestre.html"
+  }
+
+  function humanas() {
+    window.location.href = "humanas.html"
+  }
+  function natureza() {
+    window.location.href = "natureza.html"
+  }
+
+  function linguagens() {
+    window.location.href = "linguagens.html"
+  }
+
+  function matematica() {
+    window.location.href = "matematica.html"
+  }
+  function matematica() {
+    window.location.href = "matematica.html"
+  }
+
+  function ti() {
+    window.location.href = "ti.html"
+  }
+
+  function ds() {
+    window.location.href = "DS.html"
+  }
+  function ts() {
+    window.location.href = "ts.html"
+  }
+  function ms() {
+    window.location.href = "ms.html"
+  }
+  function banco() {
+    window.location.href = "banco.html"
+  }
+  function is() {
+    window.location.href = "is.html"
+  }
+  function pa() {
+    window.location.href = "pa.html"
+  }
